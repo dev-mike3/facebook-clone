be779e952aeb77c7322ad5720b84eb61cdc027a9
Michael Bamidele <michaelbamidele12345@gmail.com>
2023-07-18 14:30:47 +0100

    first commit

---
+++ myapp/src/facebook.jsx
@@ -0,0 +1,53 @@
+import  faces from './assets/facebooklogo.svg'
+export default function facebook(){
+    
+    <div className="login-fb"/>   
+    return(
+
+        <div className="body"> 
+            <div className="image">
+            <div className="fb"> <img src={faces}/> </div>
+             <span className="writeup">Facebook helps you connect and share with the people in your life.</span>
+            </div>
+             
+            
+        
+
+            <div className="formss">
+              <form action="#">
+                <input type="email" placeholder="Email or Phone number" required/>
+                <p className="text"></p>
+                <input type="password" placeholder="Password" required/>
+                <input  className="subm" type="submit" value="log in"/>
+                <a href="" className="forgot">forgotten password</a>
+                <div className="new">
+                    <p>create new account</p>
+                </div>
+                </form>
+        </div>
+        </div>
+            
+    );
+}
+
+
+
+            {/* https://github.com/dev-mike3/facebook-clone.git */}
+
+            {/* echo "# facebook-clone" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/dev-mike3/facebook-clone.git
+git push -u origin main */}
+
+         
+            
+           
+            
+               
+           
+          
+        
+       
